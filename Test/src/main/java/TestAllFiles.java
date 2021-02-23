7105c9365893ded3c78da75000a2f1b50ef497fa
LTIKanchan <50574095+LTIKanchan@users.noreply.github.com>
2021-02-23 17:26:31 +0530

    Update TestAllFiles.java

--- Test/src/main/java/TestAllFiles.java
+++ Test/src/main/java/TestAllFiles.java
@@ -3,6 +3,7 @@ public class TestAllFiles {
 	public void testthegihub(){
 		System.out.println("All changes are update...");
 		System.out.println("Lest Test it");
-		System.out.println("Pull bak from Remote Repositoy...");
+		System.out.println("Pull bak from Remote Repositoy using cmd...");
+		System.out.println("Pull back from Remote Repositoy using eclipse...");
 	}
 }
